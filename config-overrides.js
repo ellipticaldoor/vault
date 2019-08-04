182272a30d95414917ad398d2082a33e91842c95
Miguel Dorta <dortamiguel@gmail.com>
2019-08-04 08:56:48 +0100

    feat: better override config

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { paths: rewiredPaths } = require('react-app-rewired');
+const { scriptVersion } = rewiredPaths;
+const paths = require(`${scriptVersion}/config/paths`);
+
+paths.appIndexJs = path.resolve(__dirname, `src/client/index.tsx`);
+paths.appTypeDeclarations = path.resolve(
+  __dirname,
+  `src/client/react-app-env.d.ts`,
+);
+
 module.exports = function override(config) {
-  // remove eslint plugin (index #1)
+  // Disable eslint cheking
   config.module.rules.splice(1, 1);
 
   return config;
