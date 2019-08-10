d463f5dde2ac597d9fb7d1157f80b915cde6087e
Miguel Dorta <dortamiguel@gmail.com>
2019-08-10 14:24:41 +0100

    fix: getBounds test

--- src/client/screens/Atlas/draw.test.ts
+++ src/client/screens/Atlas/draw.test.ts
@@ -1,8 +1,8 @@
-import { getAtlasRuleMeasure } from 'client/screens/Atlas/draw';
+import { getBounds } from 'client/screens/Atlas/draw';
 
-describe('getAtlasRuleMeasure', () => {
-  test('returns proper rule measure', () => {
-    const coordinates = [
+test('getBounds', () => {
+  expect(
+    getBounds([
       { x: 0, y: 0 },
       { x: 10, y: 10 },
       { x: -10, y: -10 },
@@ -10,25 +10,19 @@ describe('getAtlasRuleMeasure', () => {
       { x: 10, y: 0 },
       { x: 0, y: -10 },
       { x: -10, y: 0 },
-    ];
-
-    expect(getAtlasRuleMeasure(coordinates)).toEqual({ ruleX: 20, ruleY: 20 });
-
-    expect(getAtlasRuleMeasure([])).toEqual({ ruleX: 0, ruleY: 0 });
+    ]),
+  ).toEqual({
+    x: { minPoint: -10, maxPoint: 10, distance: 20 },
+    y: { minPoint: -10, maxPoint: 10, distance: 20 },
   });
 
-  test('returns proper rule measure - 2', () => {
-    const coordinates = [
-      { x: -8, y: 42 },
-      { x: 4, y: 12 },
-      { x: 10, y: 33 },
-      { x: -3, y: 15 },
-    ];
-
-    expect(getAtlasRuleMeasure(coordinates)).toEqual({ ruleX: 18, ruleY: 30 });
+  expect(getBounds([])).toEqual({
+    x: { minPoint: 0, maxPoint: 0, distance: 0 },
+    y: { minPoint: 0, maxPoint: 0, distance: 0 },
   });
 
-  test('returns 0 if there are no coordinates', () => {
-    expect(getAtlasRuleMeasure([])).toEqual({ ruleX: 0, ruleY: 0 });
+  expect(getBounds([{ x: -3, y: 6 }, { x: 7, y: -8 }])).toEqual({
+    x: { minPoint: -3, maxPoint: 7, distance: 10 },
+    y: { minPoint: -8, maxPoint: 6, distance: 14 },
   });
 });
