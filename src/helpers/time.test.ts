12901c3c355345f45ed3f37063ef6535b4cb864c
Miguel Dorta <dortamiguel@gmail.com>
2019-08-25 13:18:50 +0100

    feat: add millisecondsToReadableTime

---
+++ src/helpers/time.test.ts
@@ -0,0 +1,19 @@
+import { ticksToTime, millisecondsToReadableTime } from '~/helpers';
+
+test('millisecondsToReadableTime', () => {
+  expect(millisecondsToReadableTime(100000000)).toEqual('27h 46m 40s');
+
+  expect(millisecondsToReadableTime(100000)).toEqual('01m 39s');
+
+  expect(millisecondsToReadableTime(10000)).toEqual('10s');
+
+  expect(millisecondsToReadableTime(2000)).toEqual('02s');
+
+  expect(millisecondsToReadableTime(1)).toEqual('00s');
+
+  expect(millisecondsToReadableTime(0)).toEqual('00s');
+});
+
+test('ticksToTime', () => {
+  expect(ticksToTime(1)).toEqual(10);
+});
---
+++ src/helpers/time.ts
@@ -0,0 +1,21 @@
+export const millisecondsToReadableTime = (milliseconds: number) => {
+  const hours = milliseconds / (1000 * 60 * 60);
+  const absoluteHours = Math.floor(hours);
+  const h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+  const minutes = (hours - absoluteHours) * 60;
+  const absoluteMinutes = Math.floor(minutes);
+  const m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
+
+  const seconds = (minutes - absoluteMinutes) * 60;
+  const absoluteSeconds = Math.floor(seconds);
+  const s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+  if (h === '00' && m === '00') return `${s}s`;
+  if (h === '00') return `${m}m ${s}s`;
+  return `${h}h ${m}m ${s}s`;
+};
+
+export const ticksToTime = (ticks: number) => {
+  return ticks;
+};
